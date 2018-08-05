d361ee57d0b1b143285336e168de549486eb249f
Georgete Bueno <geobueno@gmail.com>
2018-08-05 17:41:47 -0300

    Modal operante

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
 $(document).ready(function () {
-
   var map;
-
   function initialize() {
     var latlng = new google.maps.LatLng(-23.5576364, -46.6644888);
-
     var options = {
       zoom: 15,
       center: latlng,
@@ -12,44 +9,43 @@ $(document).ready(function () {
     };
     map = new google.maps.Map(document.getElementById("map"), options);
   }
-
   initialize();
 
-    function placesImages() {
-
+  function placesImages() {
     $.each(restaurantes, function (i, restaurante) {
-
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(restaurante.latitude, restaurante.longitude),
-        title: "Meu ponto personalizado! :-D",
-        map: map,
-
-      });
-
-      $("<img>").attr("src", restaurante.image).attr('id', restaurante.name).addClass(restaurante.type).appendTo("#images");
-
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(restaurante.latitude, restaurante.longitude),
+      title: "Meu ponto personalizado! :-D",
+      map: map,
     });
-
-
+      $("<img>").attr("src", restaurante.image).attr('id', restaurante.name).addClass(restaurante.type).appendTo("#images");
+    });
   }
-
   placesImages();
+  
+  $('img').on('click', function () {
+    var picture = $(this).attr("src");
+    var name = $(this).attr("id");
+    $("#img01").attr("src", picture);
+    $("h4").html(name);
+    $('.modal-content').load('content.html', function () {
+      $('#myModal').modal({ show: true });
+          
+    });
+    
 
-
-  $("#restaurante").on("onClick", function(){
-    alert($(this).val());
   });
 
-  $("img").click(function () {
-    var xuxu = $(this).attr("src");
-    $("#img01").attr("src", xuxu);
-  });
-});
 
-function onClick(element) {
+ });
 
-  document.getElementById("foodModal").style.display = "block";
-}
+
+
+ 
+
+
+
+
 
 
 
